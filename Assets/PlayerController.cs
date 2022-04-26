1d67292316df8d10c9f39188afdf2494522adf6c
sano_yuya <k021g1256@g.neec.ac.jp>
2022-04-26 15:14:57 +0900

    第2回確認課題

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float power = 10;
+    public Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidbody.velocity.magnitude < 4)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rigidbody.AddForce(new Vector3(0, 0, 1) * power);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rigidbody.AddForce(new Vector3(0, 0, -1) * power);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rigidbody.AddForce(new Vector3(1, 0, 0) * power);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
+            }
+        }
+    }
+}
