10db8cb63cc5b64616bf5bfeebf80b332a907694
sano_yuya <k021g1256@g.neec.ac.jp>
2022-05-10 15:08:57 +0900

    課題3

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float power = 10;
+    public float power = 1;
     public Rigidbody rigidbody;
 
     // Start is called before the first frame update
@@ -16,27 +16,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rigidbody.velocity.magnitude < 4)
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                rigidbody.AddForce(new Vector3(0, 0, 1) * power);
-            }
+            rigidbody.AddForce(new Vector3(0, 0, 1) * power);
+        }
 
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                rigidbody.AddForce(new Vector3(0, 0, -1) * power);
-            }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, -1) * power);
+        }
 
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rigidbody.AddForce(new Vector3(1, 0, 0) * power);
-            }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(new Vector3(1, 0, 0) * power);
+        }
 
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
-            }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
         }
     }
 }
